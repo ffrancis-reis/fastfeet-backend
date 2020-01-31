be61fd6171632bc6dc1b7296bcf555be179f05e6
Felipe Reis <ffrancis.reis@gmail.com>
2020-01-30 23:33:35 -0300

    Add controller package

---
+++ src/app/controllers/PackageController.js
@@ -0,0 +1,113 @@
+import * as Yup from 'yup';
+import Package from '../models/Package';
+import Recipient from '../models/Recipient';
+import DeliveryMan from '../models/DeliveryMan';
+
+class PackageController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      product: Yup.string().required(),
+      recipient_id: Yup.string(),
+      deliveryman_id: Yup.string(),
+      signature_id: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation failed' });
+    }
+
+    const { product, recipient_id, deliveryman_id, signature_id } = req.body;
+
+    const packageObj = await Package.create({
+      product,
+      recipient_id,
+      deliveryman_id,
+      signature_id,
+    });
+
+    return res.json(packageObj);
+  }
+
+  async index(req, res) {
+    const packages = await Package.findAll({
+      attributes: [
+        'id',
+        'product',
+        'recipient_id',
+        'deliveryman_id',
+        'signature_id',
+      ],
+      order: ['product'],
+    });
+
+    return res.json(packages);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      product: Yup.string(),
+      recipient_id: Yup.string(),
+      deliveryman_id: Yup.string(),
+      signature_id: Yup.string(),
+      canceled_at: Yup.date(),
+      start_date: Yup.date(),
+      end_date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation failed' });
+    }
+
+    const packageObj = await Package.findByPk(req.params.id);
+
+    if (!packageObj) {
+      return res.status(401).json({ error: 'package does not exists' });
+    }
+
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const recipientExists = await Recipient.findByPk(recipient_id);
+
+    if (!recipientExists) {
+      return res.status(401).json({ error: 'recipient does not exists' });
+    }
+
+    const deliveryManExists = await DeliveryMan.findByPk(deliveryman_id);
+
+    if (!deliveryManExists) {
+      return res.status(401).json({ error: 'delivery man does not exists' });
+    }
+
+    const {
+      product,
+      signature_id,
+      canceled_at,
+      start_date,
+      end_date,
+    } = await packageObj.update(req.body);
+
+    return res.json({
+      product,
+      recipient_id,
+      deliveryman_id,
+      signature_id,
+      canceled_at,
+      start_date,
+      end_date,
+    });
+  }
+
+  async delete(req, res) {
+    const packageObj = await Package.findByPk(req.params.id);
+
+    if (!packageObj) {
+      return res.status(401).json({ error: 'package does not exists' });
+    }
+
+    await packageObj.destroy();
+
+    return res.status(200).json(packageObj);
+  }
+}
+
+export default new PackageController();
--- src/routes.js
+++ src/routes.js
@@ -2,22 +2,29 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliveryManController from './app/controllers/DeliveryManController';
+import PackageController from './app/controllers/PackageController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
 routes.use(authMiddleware);
+
 routes.post('/recipients', RecipientController.store);
 routes.post('/deliverymen', DeliveryManController.store);
+routes.post('/packages', PackageController.store);
 
 routes.get('/recipients', RecipientController.index);
 routes.get('/deliverymen', DeliveryManController.index);
+routes.get('/packages', PackageController.index);
 
 routes.put('/recipients/:id', RecipientController.update);
 routes.put('/deliverymen/:id', DeliveryManController.update);
+routes.put('/packages/:id', PackageController.update);
 
 routes.delete('/recipients/:id', RecipientController.delete);
 routes.delete('/deliverymen/:id', DeliveryManController.delete);
+routes.delete('/packages/:id', PackageController.delete);
 
 module.exports = routes;

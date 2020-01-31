c02a1841c4b3b73bf086c55e28c2bbe6d9adf7fd
Felipe Reis <ffrancis.reis@gmail.com>
2020-01-30 22:41:17 -0300

    Add model delivery man

---
+++ src/app/models/DeliveryMan.js
@@ -0,0 +1,21 @@
+import Sequelize, { Model } from 'sequelize';
+
+class DeliveryMan extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        avatar_id: Sequelize.STRING,
+        email: Sequelize.STRING,
+      },
+      {
+        sequelize,
+        tableName: 'delivery_men',
+      }
+    );
+
+    return this;
+  }
+}
+
+export default DeliveryMan;

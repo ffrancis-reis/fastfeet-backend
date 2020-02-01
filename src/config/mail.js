b4d4e4921495cbea437a969d2d3a542faadc4620
Felipe Reis <ffrancis.reis@gmail.com>
2020-02-01 20:07:06 -0300

    Add nodemailer e config mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '9af02b5d997b7b',
+    pass: '29c46f7a33c1a4',
+  },
+  secure: false,
+  default: {
+    from: 'fastfeet staff <noreply@fastfeet.com>',
+  },
+};

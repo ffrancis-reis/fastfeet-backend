c871da8056b9e3b271b01746e60ddbe46cdcdeff
Felipe Reis <ffrancis.reis@gmail.com>
2020-02-01 18:53:25 -0300

    Add mongo config

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
 import DeliveryMan from '../app/models/DeliveryMan';
@@ -12,6 +13,7 @@ const models = [User, Recipient, DeliveryMan, Package, File, DeliveryProblem];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -20,6 +22,17 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/fastfeet',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      }
+    );
   }
 }
 
